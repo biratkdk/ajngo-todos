7311a93ac34b54a9f8fd324940c57f1319b15318
birat <biratkhadka6@gmail.com>
2023-02-03 17:30:28 +0545

    update settings

--- todosite/settings.py
+++ todosite/settings.py
@@ -129,8 +129,8 @@ EMAIL_PORT = 587
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'todosite\static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
